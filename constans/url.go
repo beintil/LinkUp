b7e49a68a7b0829b17c6ea1211bbc0750579a8d2
Vadim Savilov <vadimsavilov4@gmail.com>
2023-06-01 17:17:59 +0700

    Added new urls

--- constans/url.go
+++ constans/url.go
@@ -1,17 +1,19 @@
 package constans
 
 const (
-	Registration  = "/register"
-	Authorization = "/auth"
-	Home          = "/home"
-	HomeWithId    = "/home/:id"
-	Profile       = "/home/profile"
-	Logout        = "/logout"
-	SearchUser    = "/search"
-	GetFriends    = "/friends"
-	AddFriends    = "/friends/add"
-	DeleteFriends = "/friends/delete"
-	Chats         = "/chats"
+	Registration     = "/register"
+	Authorization    = "/auth"
+	Home             = "/home"
+	HomeWithId       = "/home/:id"
+	Profile          = "/home/profile"
+	ProfileWithId    = "/home/profile/:id"
+	Logout           = "/logout"
+	SearchUser       = "/search"
+	GetFriends       = "/friends"
+	GetFriendsWithId = "/friends/:id"
+	AddFriends       = "/friends/add"
+	DeleteFriends    = "/friends/delete"
+	Chats            = "/chats"
 )
 
 func UrlWithoutId(url string, str string) string {
